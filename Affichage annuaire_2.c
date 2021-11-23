83fe8d116a899d09fe99d9afa009f1a7d7d29cd6
Valentin <valentin.segalla@etu.univ-lyon1.fr>
2021-11-23 15:26:50 +0100

    ajout de mes programmes de départ

---
+++ Affichage annuaire_2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define annu "annuaire5000.csv"
+
+int main()
+{
+    int i;
+    char chaine[5000][300];
+    FILE * fic = fopen(annu,"a+");
+    if(fic==NULL)
+    {
+        printf("Echec Ouverture Fichier");
+        exit(EXIT_FAILURE);
+    }
+    i=0;
+    do
+    {
+        fgets(chaine[i],300,fic);
+        i++;
+    }
+    while(! feof(fic));
+    fclose(fic);
+    return 0;
+}
+
