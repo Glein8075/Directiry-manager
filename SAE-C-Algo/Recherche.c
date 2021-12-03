5900bab4b7c0279c0a7a1a29efbad06b4b23be28
Valentin <valentin.segalla@etu.univ-lyon1.fr>
2021-12-03 12:01:24 +0100

    modification de la fonction de recherche

--- SAE-C-Algo/Recherche.c
+++ SAE-C-Algo/Recherche.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
-int recherche(int t1, int t2, char tab[t1][t2], char mot)
+int recherche(int t1, int t2, char tab[t1][t2], int indice, char mot)
 {
-
+    int i,j,milieu;
+    i=0;
+    j=t1;
+    while i<j
+    {
+        milieu=(i+j)/2;
+        if (strcmp(tab[milieu][indice],mot)==0)
+            return milieu;
+        if (strcmp(tab[milieu][indice],mot)<0)
+            j=milieu-1
+        else i=milieu +1
+    }
+    return -1;
 }
