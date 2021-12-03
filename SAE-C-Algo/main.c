12f5da16fed0a5ab080ec703fa7e43ccb257826b
MIAUX Melvin <melvin.miaux@etu.univ-lyon1.fr>
2021-12-03 10:36:01 +0100

    Ajout interface prototype

--- SAE-C-Algo/main.c
+++ SAE-C-Algo/main.c
@@ -1,8 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct
+{
+    char nom[20];
+    char prenom[20];
+    char numtel[12];
+}client;
+
 int main()
 {
 
+
+
+    int choix;
+    do
+    {
+        printf("MENU\n\n");
+        printf("Ajouter une personne a l'annuaire ....... 1\n");
+        printf("Afficher l'annuaire ..................... 2\n");
+        printf("Supprimer une personne de l'annuaire .... 3\n");
+        printf("Modifier une personne de l'annuaire ..... 4\n");
+        printf("Quitter ................................. 5\n");
+        printf("Votre choix :");
+        scanf("%d", &choix);
+        switch(choix)
+        {
+            case '1': printf("1");
+            case '2': printf("2");
+            case '3': printf("3");
+            case '4': printf("4");
+            case '5': break;
+            default : printf("Erreur de saisie !\n" );
+        }
+    }while(choix!= '5');
+
     return 0;
 }
+
