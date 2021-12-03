08d0285abafcda41e3f6bbf65aef9bca32ae250f
MIAUX Melvin <melvin.miaux@etu.univ-lyon1.fr>
2021-12-03 11:12:38 +0100

    Ajout du tri indirect et test

---
+++ SAE-C-Algo/Tri indirect.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "en-tete.h"
+
+/**
+:entree ad:tableau de "x" 1 dimension
+:entre/sortie  ai:tableau de "x" 2 dimensions
+:Declaration
+:i,j:int
+:ipetit:int
+**/
+
+void tri_indirect()
+{
+        int i=1,ipetit;
+        while (i <= ad.size-1)
+        {
+            ipetit=ai[i];
+            j=i-1;
+            while (j >= 0 && ad[ipetit] < ad[ai[j])
+            {
+                ai[j+1]=ai[j];
+                j++;
+            }
+            ai[j+1]=ipetit;
+            i++;
+        }
+}
+
+int main()
+{
+    ["","Becker","PARIS","75002","01.81.30.33.94","adelaide.becker@server.fr","comptable"]
+    ["Benoit","Lambert","","","04.61.87.21.94","benoit.lambert@server.fr","chef d'entreprise"]
+    ["Capucine","Humbert du Leroux","MARSEILLE","13008","04.13.88.22.43","capucine.humbert-du-leroux@server.fr","barman"]]}
+}
--- SAE-C-Algo/en-tete.h
+++ SAE-C-Algo/en-tete.h
@@ -1,6 +1,6 @@
 #ifndef EN-TETE_H_INCLUDED
 #define EN-TETE_H_INCLUDED
 
-
+char separation(char chaine[300]);
 
 #endif // EN-TETE_H_INCLUDED
